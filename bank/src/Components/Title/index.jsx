8d156053997b4ae19b375934efa72a1a92e81784
selvatico87 <ignacioselvatico@gmail.com>
2023-04-17 14:33:35 -0300

    inicio
    A instalar el Styled Components dentro de nuestro proyecto;
    A como crear un componente utilizando Styled Components;
    Sobre la generación automática de clases;
    A cómo transferir código CSS para mí componente estilizado.

---
+++ bank/src/Components/Title/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components"
+
+const Title = styled.h1`
+color: grey;
+padding: 25px 0;
+`
+// const Title = ({ children }) => {
+//   return <h1 className="title">{children}</h1>;
+// };
+export default Title;
