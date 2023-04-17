2358b7db6adb8baaf9f5787ee39e7ed853c564a2
selvatico87 <ignacioselvatico@gmail.com>
2023-04-17 14:55:04 -0300

    17/4.1

--- bank/src/Components/Header/index.jsx
+++ bank/src/Components/Header/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import logo from "../../assets/images/logo.svg";
+import styled from "styled-components";
+import { colorPrimario } from "../Ui/Variables";
+
+
+const StyledHeader = styled.nav`
+  background-color: ${colorPrimario};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`
 
 const Header = () => {
   return (
-    <div className="header">
-      <img className="imagen-logo" src={logo} alt="Logo Smart Bank" />
+    <StyledHeader >
+      <Logo src={logo} alt="Logo Smart Bank" />
       <div>
         <a className="btn-secundario" href="https://google.com">
           Ayuda
@@ -13,7 +29,7 @@ const Header = () => {
           Salir
         </a>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 
