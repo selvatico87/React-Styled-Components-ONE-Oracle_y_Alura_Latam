bfc34c4c571eeb586a136ba821bcbeeb687c8d8e
selvatico87 <ignacioselvatico@gmail.com>
2023-04-18 12:55:42 -0300

    18/4.2
    Porqué utilizar inline styles no es bueno;
    Cómo utilizar Herencia de estilo;
    Sobre los posibles problemas de performance caso un Styled Component esté declarado dentro de otro componente React;
    Cómo tratar de medias query dentro de los componentes estilizados.

--- bank/src/Components/Container/index.jsx
+++ bank/src/Components/Container/index.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-
+import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
 
+const StylesContainer= styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+const StyledContent=styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px){
+    flex-direction: column;
+  };
+`
+
 const Container = () => {
   return (
-    <div className="container">
+    <StylesContainer>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StyledContent>
         <Account />
-      </section>
-    </div>
+      </StyledContent>
+    </StylesContainer>
   );
 };
 
