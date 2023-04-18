ce6f85e29aade38bc748b6acccdc54991ac22762
selvatico87 <ignacioselvatico@gmail.com>
2023-04-18 12:26:27 -0300

    18/4.1
    Utilizar el reset global del proyecto con Styled Components;
    Crear estilos globales para su aplicación;
    Importar fontes de google para nuestro proyecto;
    Crear variables para colores del proyecto;
    Utilizar propiedades en Componentes estilizados.

--- bank/src/Components/Header/index.jsx
+++ bank/src/Components/Header/index.jsx
@@ -16,18 +16,27 @@ const Logo = styled.img`
   height: 50px;
   width: 50px;
 `
-
+const BtnHeader =styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  color: ${(props)=>props.primary ? "white": colorPrimario};
+  background: ${(props)=> props.primary?"transparent":"white" }
+`
 const Header = () => {
   return (
     <StyledHeader >
       <Logo src={logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnHeader  href="https://google.com">
           Ayuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnHeader>
+        <BtnHeader primary href="https://google.com">
           Salir
-        </a>
+        </BtnHeader>
       </div>
     </StyledHeader>
   );
