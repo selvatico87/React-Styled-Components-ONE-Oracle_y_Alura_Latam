0bacf91b2b823ff825d3fcbe3dd720a8b9f9cd36
selvatico87 <ignacioselvatico@gmail.com>
2023-04-19 14:14:19 -0300

    final

--- bank/src/Components/SwitcherTema/SwitcherTema/index.js
+++ bank/src/Components/SwitcherTema/SwitcherTema/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import themeOn from "../../assets/images/themeOn.svg";
-import themeOff from "../../assets/images/themeOff.svg";
-import { Icono } from "../UI";
+import themeOn from "./assets/images/themeOn.svg";
+import themeOff from "./assets/images/themeOff.svg";
+import { Icono } from "./UI";
 
 export default ({ tema }) => {
   const claro = <Icono src={themeOn} alt="Tema claro" />;
